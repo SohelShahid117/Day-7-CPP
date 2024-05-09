71af2b26253c9a63be86976436c5187c3f783c28
SohelShahid117 <sohelshahid09@gmail.com>
2024-05-09 14:46:33 +0600

    up to t-11-oprtator done

--- escape.cpp
+++ escape.cpp
@@ -5,7 +5,30 @@ int main()
 {
     cout << "hello sohel \t \\ shaid" << endl;
     cout << "ctg\?,\a \" BD\n";
-    cout << "01823012335";
+    cout << "01823012335\n";
+    string a = "aaaaaaaaaaaaaaaa\n";
+    cout << a;
+
+    char b[] = "aaaaaaaaaaaaaaaa\n";
+    cout << b;
+
+    int num;
+    cout << "enter a number :";
+    cin >> num;
+    cout << "enter number is :" << num;
+
+    int num1, num2;
+    cout << "\n enter two number : ";
+    cin >> num1 >> num2;
+    int sum = num1 + num2;
+    cout << "sum is : " << sum << endl;
+    int mul = num1 * num2;
+    cout << "mul = " << mul << endl;
+    double div = (float)num1 / num2; // type casting
+    cout << "div = " << div << endl;
+
+    double mod = num1 % num2; // type casting
+    cout << "div = " << mod << endl;
     return 0;
 }
 /*
