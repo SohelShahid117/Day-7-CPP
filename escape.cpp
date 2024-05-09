56f798392f3797582aafec25ab72c6dfad4797dd
SohelShahid117 <sohelshahid09@gmail.com>
2024-05-09 10:57:56 +0600

    nJoy Programming lif-CPP-escape sequence

---
+++ escape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// C++ Bangla Tutorials 5 : Escape sequence
+int main()
+{
+    cout << "hello sohel \t \\ shaid" << endl;
+    cout << "ctg\?,\a \" BD\n";
+    cout << "01823012335";
+    return 0;
+}
+/*
+https://youtu.be/9zL8nm7mnsI?si=eQBnTQawZGUwRqxd
+*/
