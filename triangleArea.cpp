be56edf6a2b6fd79a9e58750d377303984b34eb9
SohelShahid117 <sohelshahid09@gmail.com>
2024-05-09 19:42:58 +0600

    done area of circle

--- triangleArea.cpp
+++ triangleArea.cpp
@@ -3,7 +3,9 @@ using namespace std;
 int main()
 {
     // cout << "hello world";
-    double Area, base, height;
+    double Area, base, height, circleArea;
+    float pi = 3.1416;
+    float r;
 
     cout << "enter base:";
     cin >> base;
@@ -14,5 +16,13 @@ int main()
     cout << endl;
     Area = (float)1 / 2 * base * height;
     cout << "area of triangle" << Area;
+    cout << endl;
+
+    cout << "enter the radius of circle : ";
+    cin >> r;
+    cout << endl;
+    circleArea = pi * r * r;
+    cout << "area of circle " << circleArea;
+
     return 0;
 }
